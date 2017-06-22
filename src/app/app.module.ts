8139fce4e30a7e781432ae252505fb566a09d845
Shreeharsh S Ambli <Shreeharsh.Ambli@neudesic.com>
2017-06-22 18:39:26 +0530

    added new star rating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StarRatingComponent } from './star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarRatingComponent
   ],
   imports: [
     BrowserModule
